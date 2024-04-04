0f4f6b8dc37bcf156c35994bbf356ea2edaf6cc4
Keith Warter <kmwarter@gmail.com>
2024-04-03 17:04:36 -0700

    Some improvements

--- README.md
+++ README.md
@@ -6,5 +6,3 @@ The Website for Chappell Build Co.
 
 - Ideal to get original PSD assets to improve illustrations
 - Needs mobile responsiveness
-- Needs social links on contact page
-- Could improve with a contact button but that requires a server most likely
---
+++ projects.js
@@ -0,0 +1,38 @@
+const imageExists = async (url) => {
+  try {
+    const response = await fetch(url, { method: 'HEAD' });
+    return response.ok;
+  } catch (error) {
+    return false;
+  }
+};
+
+(async () => {
+  // While images exist in the projects folder to display we will display them
+  let imageNumber = 1;
+  while (imageNumber) {
+    const assumedSrc = `images/projects/image_${imageNumber}.jpg`
+    const imgExists = await imageExists(assumedSrc);
+
+    if (imgExists) {
+      // Create a new image element 
+      var img = document.createElement('img'); 
+      img.src = assumedSrc;
+      img.className = 'project-image';
+      
+      // Get two column elements
+      const columns = document.getElementsByClassName("column");
+      
+      // Place images into two columns
+      if (imageNumber % 2 !== 0) {
+        columns[0].appendChild(img);
+      } else {
+        columns[1].appendChild(img);
+      }
+
+      imageNumber++
+    } else {
+      imageNumber = 0;
+    }
+  }
+})()
